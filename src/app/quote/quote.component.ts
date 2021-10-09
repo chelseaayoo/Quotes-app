83cb9f96815b38837271d2585b697d7dab1dd829
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-10-09 06:16:02 -0400

    move quote array to quote component class

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes:Quote[] =[
+    {id:1, name:'Life hacks, try coding!'},
+    {id:2, name:'Make it work.make it right and fast!'},
+    {id:3, name:'Before software can be reusable it first has to be usable.'},
+    {id:4, name:'Talk is cheap. Show me the code!'},
+    {id:5, name:'You can turn coffee into code!'}
+
+
+  ]
 
   constructor() { }
 
