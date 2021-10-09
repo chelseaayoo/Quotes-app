f2949971efc49718e3b4449b8ed91cf99fe75401
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-10-09 05:45:25 -0400

    create several quotes using the quote blueprint class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Quotes';
+export class AppComponent{
+  quotes:Quote[] =[
+    {id:1, name:'Life hacks, try coding!'},
+    {id:2, name:'Make it work.make it right and fast!'},
+    {id:3, name:'Before software can be reusable it first has to be usable.'},
+    {id:4, name:'Talk is cheap. Show me the code!'},
+    {id:5, name:'You can turn coffee into code!'}
+
+
+  ]
 }
