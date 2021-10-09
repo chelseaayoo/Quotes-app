4eb63e2d34959cd6252d923ff23f8ec189a570bf
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-10-09 11:21:45 -0400

    add output binding property

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,6 @@
 export class Quote {
-    id!: number;
-    name!: string;
-    author!: string;
+    showAuthor: boolean;
+  constructor(public id: number,public name: string,public author: string){
+    this.showAuthor=false;
+}
 }
